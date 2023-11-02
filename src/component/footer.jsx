04c931e933856f8d8fc58f4df0383123b4b77c75
jean-claude <jean-claude.grech@orange.fr>
2023-11-02 19:29:22 +0100

    responsive

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import FootLogo from '../assets/FootLogo.png';
-import AllRights from '../assets/AllRights.png';
-import '../styles/footer.scss'
-
+import React from "react";
+import FootLogo from "../assets/FootLogo.png";
+import AllRights from "../assets/AllRights.png";
+import "../styles/footer.scss";
 
 const Footer = () => {
-    return (
-        <div>
-            
-            <div className="footer-container">
-                <img src={FootLogo} alt="logo kasa" />
-                <img src={AllRights} alt="rights"/>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div className="footer-container">
+				<div className="footer-container__items">
+					<img src={FootLogo} alt="logo kasa" />
+					<img src={AllRights} alt="rights" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
-export default Footer
+export default Footer;
